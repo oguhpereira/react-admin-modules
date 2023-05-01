2b4be82c9d2f2006d9e5861d044b52190c86a10d
oguhpereira <oguhpereira@protonmail.com>
2023-05-01 13:23:45 -0300

    Create Nx exemple

---
+++ nx/packages/my-new-app/src/app/app.tsx
@@ -0,0 +1,35 @@
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@mui/icons-material/Book';
+import UserIcon from '@mui/icons-material/Group';
+
+import { UserList } from '@nx/user-team';
+import { authProvider } from './authProvider';
+import { Dashboard } from './Dashboard';
+import { PostList, PostEdit, PostCreate } from './posts';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+    >
+        <Resource
+            name="posts"
+            list={PostList}
+            edit={PostEdit}
+            create={PostCreate}
+            icon={PostIcon}
+        />
+        <Resource
+            name="users"
+            list={UserList}
+            icon={UserIcon}
+            recordRepresentation="name"
+        />
+    </Admin>
+);
+
+export default App;
